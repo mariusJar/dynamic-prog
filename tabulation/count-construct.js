71a4f0071bf34d3cbf3db5dd2f51626a712fd776
mariusJar <marius.jaraminas@gmail.com>
2021-03-14 22:54:59 +0200

    countConstruct

--- tabulation/count-construct.js
+++ tabulation/count-construct.js
@@ -1,22 +1,21 @@
 // Write a function countConstruct(target, wordBank) that accepts a target string and an array of strings.
-// This function should return a number of ways that the target string can be constructed by concatinating elements of the wordBank array.
+// This function should return a number of ways that the target string can be constructed
+// by concatinating elements of the wordBank array.
 // You may reuse elements of "wordBank" as many times as needed.
 
-const countConstruct = (target, wordBank, memo = {}) => {
-  if (target in memo) return memo[target]
-  if (target === '') return 1
+const countConstruct = (target, wordBank) => {
+  const table = Array(target.length + 1).fill(0)
+  table[0] = 1
 
-  let totalCount = 0
-
-  for (let word of wordBank) {
-    if (target.indexOf(word) === 0) {
-      const numWays = countConstruct(target.slice(word.length), wordBank, memo)
-      totalCount += numWays
+  for (let i = 0; i <= target.length; i++) {
+    for (let word of wordBank) {
+      if (target.slice(i, i + word.length) === word) {
+        table[i + word.length] += table[i]
+      }
     }
   }
 
-  memo[target] = totalCount
-  return totalCount
+  return table[target.length]
 }
 
 console.log(countConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl'])) // 2
